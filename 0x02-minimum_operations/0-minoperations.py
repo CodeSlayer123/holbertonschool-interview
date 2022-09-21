32ff2c5c1f39731e18e687a1aef9b08f08a03d3a
Code Slayer <3613@holbertonschool.com>
2022-09-21 16:44:17 -0500

    prime test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -49,8 +49,5 @@ def is_prime(n):
     if n > 1:
         for i in range(2, int(n/2)+1):
             if n % i == 0:
-                break
-            else:
-                return True
-    else:
+                return False
         return False
