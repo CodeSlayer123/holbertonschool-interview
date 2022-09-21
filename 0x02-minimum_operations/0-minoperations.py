359972dc1037fbed56d41b26e1b6eedb5e29dd5e
Code Slayer <3613@holbertonschool.com>
2022-09-21 16:48:25 -0500

    rewind time

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,23 +1,79 @@
 #!/usr/bin/python3
-""" minimum operations """
+"""Computes minimum amount of operations needed to reach n letters of H"""
+
+H = "H"
+chars = 1
+operations = 0
+copied = ""
 
 
 def minOperations(n):
-    """ calculates min ops to result in n H """
+    """main function that computes number of operations needed"""
+    global H
+    global chars
+    global operations
+    global copied
+
+    H = "H"
+    operations = 0
+    copied = ""
+    chars = 1
 
     if n <= 1:
         return 0
-
-    file = ['H']
-    count = len(file)
-    if isPrime(n):
+    if is_prime(n):
         return n
+    copy_all()
+    paste()
+
+    for i in range(n):
+        if chars < n:
+            chars_needed = n - chars
+            if (chars_needed) % 2 != 0:
+                # number of chars needed to be added is odd
+                paste()
+            if chars != n:
+                if (chars - (n - chars) == 1):
+                    for i in range(n - chars):
+                        paste()
+                    return operations
+                # print("chars", chars)
+                # print("chars needed", n - chars)
+                # print()
+                copy_all()
+
+                while ((n - chars) - chars > 4):
+                    paste()
+                    copy_all()
+                for i in range((n - chars) // chars):
+                    paste()
+
+    return operations
+
+def copy_all():
+    """copy all letters currently printed"""
+    global copied
+    global operations
+
+    copied = H
+    operations += 1
 
 
-def isPrime(n):
-    """ checks if number is prime """
-    for i in range(2, int(n/2) + 1):
-        if (n % i) == 0:
-            return False
-    return True
+def paste():
+    """pastes all letters stored from the previous copy_all function call"""
+    global copied
+    global operations
+    global H
+    global chars
 
+    operations += 1
+    H += copied
+    chars = len(H)
+    return H
+
+def is_prime(n):
+    if n > 1:
+        for i in range(2, int(n/2)+1):
+            if n % i == 0:
+                return False
+        return True
