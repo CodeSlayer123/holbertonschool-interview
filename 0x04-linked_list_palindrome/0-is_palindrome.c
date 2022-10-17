f5d436de3ec1bdb4c9a23dc6f0af49d08d12d6da
Code Slayer <3613@holbertonschool.com>
2022-10-17 11:29:08 -0500

    first commit

---
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+int is_palindrome(listint_t **head){
+    if (*head == NULL){
+        return 1;
+    }
+    listint_t **first = head;
+    listint_t *last = *first;
+    listint_t **tmp = first;
+
+    last = last->next;
+    while(last->next != NULL){
+        last = last->next;
+        *tmp = (*tmp)->next;
+
+    }
+    while ((*first)->next != NULL && last != *head){
+        if ((*first)->n != last->n){
+            return 0;
+        }
+        *first = (*first)->next;
+        last = *tmp;
+        tmp = head;
+        if (*tmp != last){
+            while ((*tmp)->next != last){
+                *tmp = (*tmp)->next;
+            }
+        }
+
+    }
+    return 1;
+
+}
