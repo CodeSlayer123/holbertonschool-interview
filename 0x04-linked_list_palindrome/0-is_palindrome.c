ca66ea329c3a5555fc62b15ad22a442553e1e678
Code Slayer <3613@holbertonschool.com>
2022-10-18 09:09:22 -0500

    single pointer tmp

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -6,24 +6,25 @@ int is_palindrome(listint_t **head){
     }
     listint_t **first = head;
     listint_t *last = *first;
-    listint_t **tmp = first;
+    listint_t *tmp = *first;
 
     last = last->next;
     while(last->next != NULL){
         last = last->next;
-        *tmp = (*tmp)->next;
+        tmp = tmp->next;
 
     }
     while ((*first)->next != NULL && last != *head){
+        printf("test");
         if ((*first)->n != last->n){
             return 0;
         }
         *first = (*first)->next;
-        last = *tmp;
-        tmp = head;
-        if (*tmp != last){
-            while ((*tmp)->next != last){
-                *tmp = (*tmp)->next;
+        last = tmp;
+        tmp = *head;
+        if (tmp != last){
+            while (tmp->next != last){
+                tmp = tmp->next;
             }
         }
 
