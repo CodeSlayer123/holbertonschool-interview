3d2cc4c044742c51c571842ae0c54a7eb04d829b
Code Slayer <3613@holbertonschool.com>
2022-12-07 10:56:10 -0600

    meneger runs locally

---
+++ 0x0A-menger/0-menger.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "menger.h"
+
+void menger(int level){
+    int size = pow(3, level);
+    int i;
+    int j;
+    int z;
+
+    for (int y = 0; y < size; y++) {
+
+        for (int x = 0; x < size; x++) {
+            i = x;
+            j = y;
+
+            for(z = 0; z < level; z++){
+                if (i % 3 == 1 && j % 3 ==1){
+                    break;
+                }
+                i /= 3;
+                j /= 3;
+            }
+            if (z == level){
+                printf("#");
+            }
+            else {
+                printf(" ");
+            }
+
+        }
+
+        printf("\n");
+    }
+
+}
