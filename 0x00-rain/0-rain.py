a946e2116577ae4af5e5c53eeebcc71ac4fa8aaa
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-09 17:07:13 +0000

    empy walls and for loop revised

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -3,18 +3,19 @@
 0_rain
 """
 def rain(walls):
+    
+    if len(walls) <= 0:
+        return 0
     total = 0
+    print(walls)
     for i in walls:
         if i == 0:
             walls.remove(i)
-
     for i in walls:
         if i == 0:
             walls.remove(i)
 
     size = len(walls)
-    last = walls[size - 1]
-    for j in walls:
-        if j != last:
-            total += j
+    for j in range(size - 1):
+        total += walls[j]
     return total
