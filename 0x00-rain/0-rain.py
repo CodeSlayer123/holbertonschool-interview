9dc052ffd740f26f84e02d0bdb902ab749badb12
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-01-09 16:43:55 +0000

    runs locally

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -4,25 +4,17 @@
 """
 def rain(walls):
     total = 0
-    print(walls)
     for i in walls:
         if i == 0:
-            print(i, "yes")
             walls.remove(i)
-        else:
-            print(i, "no")
-    
+
     for i in walls:
         if i == 0:
-            print(i, "yes")
             walls.remove(i)
-        else:
-            print(i, "no")
-    print(walls)
+
     size = len(walls)
     last = walls[size - 1]
     for j in walls:
         if j != last:
             total += j
-    print(total)
     return total
