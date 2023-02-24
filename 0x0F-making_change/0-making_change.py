cba02ecb7595951c324b48c2a9605d2e4f32bb6b
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-02-24 15:28:27 +0000

    version 1

--- 0x0F-making_change/0-making_change.py
+++ 0x0F-making_change/0-making_change.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
 
 def makeChange(coins, total):
-    
+    new_total = total
+    change = 0
+    coins.sort(reverse=True)
+    if (new_total - coins[0] >= 0):
+        new_total -= coins[0]
+        change += 1
     for coin in coins:
-        print(coin)
-    print(total)
+        while(1):
+            if (new_total - coin >= 0 and new_total - coin >= coin):
+                new_total -= coin
+                change += 1
+            else:
+                break
+    if (new_total > 1):
+        return -1
+    return change
