e6ad2f69f2e17b3d6b2788d0122a30ec0608be96
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-03-06 22:22:27 +0000

    island algo

---
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,14 @@
+def island_perimeter(grid):
+    total = 0
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if (grid[i][j] == 1):
+                if (grid[i][j+1] == 0):
+                    total += 1
+                if (grid[i][j-1] == 0):
+                    total += 1
+                if (grid[i-1][j] == 0 ):
+                    total += 1
+                if (grid[i+1][j] == 0):
+                    total += 1
+    return total
