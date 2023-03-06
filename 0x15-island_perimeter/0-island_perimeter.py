da5a763f5bb6e84dea48797d0af13f00d6e3fe7c
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-03-06 22:36:50 +0000

    added null checks

--- 0x15-island_perimeter/0-island_perimeter.py
+++ 0x15-island_perimeter/0-island_perimeter.py
@@ -9,12 +9,21 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid)):
             if (grid[i][j] == 1):
-                if (grid[i][j+1] == 0):
-                    total += 1
-                if (grid[i][j-1] == 0):
-                    total += 1
-                if (grid[i-1][j] == 0 ):
-                    total += 1
-                if (grid[i+1][j] == 0):
-                    total += 1
+
+                if (j != len(grid)):
+                    if (grid[i][j+1] == 0):
+                        total += 1
+
+                if (j != 0):
+                    if (grid[i][j-1] == 0):
+                        total += 1
+
+                if (i != len(grid)):
+                    if (grid[i-1][j] == 0 ):
+                        total += 1
+
+                if (i != 0):
+                    if (grid[i+1][j] == 0):
+                        total += 1
+
     return total
