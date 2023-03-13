4f5846e751d6fc02593dc5cbb87685b5c7caf233
CodeSlayer123 <90207564+CodeSlayer123@users.noreply.github.com>
2023-03-13 20:37:29 +0000

    prime game

--- 0x1C-primegame/0-prime_game.py
+++ 0x1C-primegame/0-prime_game.py
@@ -1,6 +1,51 @@
 #!/usr/bin/python3
 
+def is_prime(num):
+    flag = False
+
+    if num == 1:
+        return(False)
+    elif num > 1:
+        # check for factors
+        for i in range(2, num):
+            if (num % i) == 0:
+                # if factor is found, set flag to True
+                flag = True
+                # break out of loop
+                break
+
+        # check if flag is True
+        if flag:
+            return False
+        else:
+            return True
 
 def isWinner(x, nums):
-    print(x)
-    print(nums)
+    rounds = x
+    round = 0
+    test = [2,2, 3, 4]
+    to_remove = []
+    Ben = 0
+    Maria = 0
+        
+    for i in nums:
+        round += 1
+        if is_prime(i):
+            # print("yes!")
+            for j in nums:
+                if (j % i == 0):
+                    # print("i is", i)
+                    # print("j is", j)
+                    to_remove.append(j)
+    # print(to_remove)
+    for remove in to_remove:
+        try:
+            while True:
+                nums.remove(remove)
+        except ValueError:
+            pass
+ 
+    
+    return 'Ben'
+    # print(x)
+    # print(nums)
